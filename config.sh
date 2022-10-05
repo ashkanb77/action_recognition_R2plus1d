5e396a4d1dec9caf4897383484c6c257ceaefd69
ashkan <bagheriashkan77@gmail.com>
2022-10-05 21:01:57 +0330

    Add files via upload

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 ! mkdir "dataset"
 
 ! wget --no-check-certificate https://www.crcv.ucf.edu/data/UCF101/UCF101.rar
-! unrar x -Y "/content/UCF101.rar" "dataset"
+! unrar x -Y "/content/action_recognition_R2plus1d/UCF101.rar" "dataset"
 
 ! wget --no-check-certificate https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
-! unzip '/content/UCF101TrainTestSplits-RecognitionTask.zip' -d 'dataset'
+! unzip '/content/action_recognition_R2plus1d/UCF101TrainTestSplits-RecognitionTask.zip' -d 'dataset'
